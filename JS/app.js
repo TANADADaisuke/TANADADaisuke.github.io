aa04deb91c8cbf3a2fe5e8cde9441915e21e0006
Tanada Daisuke <nvkx873110@gmail.com>
2019-12-25 18:43:43 +0900

    Add open action

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,23 @@
 // vocabulary.close action
 $('.fa-times').on('click', function() {
-    $(this).parent().addClass('close');
+    $(this).parent().removeClass('open');
 });
+
+// vocabulary.open action
+$('.content').on('click', 'span', function(event) {
+    // targetText is a word or words in a paragraph
+    // which meaning you want to know
+    var targetText;
+    targetText = $(event.target).text();
+    console.log(targetText);
+    // find vocabulary card from hidden list and display it
+    $('.vocabulary').each(function() {
+        // text is a title of each vocabulary card
+        var text;
+        text = $(this).children().first().text();
+        // if text is the one you want, display the card
+        if (targetText === text) {
+            $(this).addClass('open');
+        }
+    });
+})
