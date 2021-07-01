f66feaaa4cbee5df40a4fe204091c07c12cd706b
TANADADaisuke <nvkx873110@gmail.com>
2021-07-02 06:35:23 +0900

    fix: Remove alert

--- portfolio/javascript/counter-momentum-scroll/app.js
+++ portfolio/javascript/counter-momentum-scroll/app.js
@@ -108,7 +108,7 @@ let scrollDistance = 200;
 function renderScrollAction(timestamp) {
     if (!start) start = timestamp;
     const progress = timestamp - start;
-    alert(progress);
+    // alert(progress);
     // // scrollTop overwrite
     scrollArealeft.firstElementChild.style.transform = 'translateY(-' + Math.min(progress, 2000) + 'px)';
     scrollAreaRight.firstElementChild.style.transform = 'translateY(' + Math.min(progress, 2000) + 'px)';
