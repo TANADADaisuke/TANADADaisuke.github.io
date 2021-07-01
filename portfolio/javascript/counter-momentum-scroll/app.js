cffc569b022fa5a51d92540c0fe2ec9b43e25927
TANADADaisuke <nvkx873110@gmail.com>
2021-07-01 16:07:16 +0900

    feat: Prevent touch move on touch device

--- portfolio/javascript/counter-momentum-scroll/app.js
+++ portfolio/javascript/counter-momentum-scroll/app.js
@@ -4,3 +4,26 @@ const scrollAreaRightContent = scrollAreaRight.firstElementChild;
 
 // set container element scrollTop property
 scrollAreaRight.scrollTop = scrollAreaRightContent.getBoundingClientRect().height;
+
+
+// prevent touch swipe action
+// functions
+function handleTouchStart(event) {
+    // prevent default
+    event.preventDefault();
+    // add touchmove and touchend event listener
+    window.addEventListener('touchmove', handleTouchMove, {passive: false});
+    window.addEventListener('touchend', handleTouchEnd, {passive: false});
+}
+
+function handleTouchMove(event) {
+    console.log(event.target)
+}
+
+function handleTouchEnd(event) {
+    // remove touchmove and touchend listener
+    window.removeEventListener('touchmove', handleTouchMove, {passive: false});
+    window.removeEventListener('touchend', handleTouchEnd, {passive: false});
+}
+
+window.addEventListener('touchstart', handleTouchStart, {passive: false});
