753f686791bb4306320ece694723b65c8ef1e1e2
TANADADaisuke <nvkx873110@gmail.com>
2021-07-01 16:14:42 +0900

    refactor: Best practive for touch event

--- portfolio/javascript/counter-momentum-scroll/app.js
+++ portfolio/javascript/counter-momentum-scroll/app.js
@@ -1,4 +1,5 @@
 // scroll right scroll area to the bottom on page load
+const headerContainer = document.querySelector('.header-container');
 const scrollAreaRight = document.querySelector('.scroll-area-right');
 const scrollAreaRightContent = scrollAreaRight.firstElementChild;
 
@@ -6,14 +7,14 @@ const scrollAreaRightContent = scrollAreaRight.firstElementChild;
 scrollAreaRight.scrollTop = scrollAreaRightContent.getBoundingClientRect().height;
 
 
-// prevent touch swipe action
+// prevent touch swipe action on header container
 // functions
 function handleTouchStart(event) {
     // prevent default
     event.preventDefault();
     // add touchmove and touchend event listener
-    window.addEventListener('touchmove', handleTouchMove, {passive: false});
-    window.addEventListener('touchend', handleTouchEnd, {passive: false});
+    headerContainer.addEventListener('touchmove', handleTouchMove, {passive: false});
+    headerContainer.addEventListener('touchend', handleTouchEnd, {passive: false});
 }
 
 function handleTouchMove(event) {
@@ -22,8 +23,8 @@ function handleTouchMove(event) {
 
 function handleTouchEnd(event) {
     // remove touchmove and touchend listener
-    window.removeEventListener('touchmove', handleTouchMove, {passive: false});
-    window.removeEventListener('touchend', handleTouchEnd, {passive: false});
+    headerContainer.removeEventListener('touchmove', handleTouchMove, {passive: false});
+    headerContainer.removeEventListener('touchend', handleTouchEnd, {passive: false});
 }
 
-window.addEventListener('touchstart', handleTouchStart, {passive: false});
+headerContainer.addEventListener('touchstart', handleTouchStart, {passive: false});
